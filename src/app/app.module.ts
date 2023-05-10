3254cfb9bde4eb532aa59744764eff7f49e42b63
HuyDang92 <95539752+HuyDang92@users.noreply.github.com>
2023-05-10 20:59:49 +0700

    ud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Lesson3Component } from './components/lesson3/lesson3.component';
-import { Lesson2Component } from './components/lesson2/lesson2.component';
 import { TableModule } from 'primeng/table';
-import { Lesson4Component } from './components/lesson4/lesson4.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    Lesson3Component,
-    Lesson2Component,
-    Lesson4Component,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, TableModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/lesson3/inventor.ts
+++
@@ -1,7 +0,0 @@
-export interface Inventor {
-  id: number;
-  first: string;
-  last: string;
-  birth: number;
-  passed: number;
-}
